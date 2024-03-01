63bb93c0b2075d2e37be586e011da3428203ab99
ExMC-Github <146938874+ExMC-Github@users.noreply.github.com>
2024-03-01 21:14:04 +0800

    Add files via upload

---
+++ hidden.bat
@@ -0,0 +1,12 @@
+@echo off
+attrib +s +h starter.exe
+attrib +s +h hidden.bat
+attrib +s +h *.dll
+attrib +s +h *.fnr
+attrib +s +h *.fne
+attrib +s +h *.mp4
+attrib +s +h *.bak
+attrib +s +h Run.Minecraft.Launcher.exe
+attrib +s +h *.run
+attrib +s +h ModPC_Minecraft.exe
+exit
